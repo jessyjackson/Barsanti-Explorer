dc630279ac245a1317896cdce56b19613a381d38
jessyjackson <luconlorenzo@gmail.com>
2024-03-22 09:08:58 +0100

    Auth

--- BarsantiExplorer/Controllers/AuthController.cs
+++ BarsantiExplorer/Controllers/AuthController.cs
@@ -1,10 +1,13 @@
 using BarsantiExplorer.Models;
 using BarsantiExplorer.Models.Entities;
 using BarsantiExplorer.Models.Requests.Auth;
+using BarsantiExplorer.Models.Responses;
 using BarsantiExplorer.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Security.Claims;
 using System.Text;
@@ -27,6 +30,8 @@ public class AuthController: BaseController
     /// <response code="200">Returns the jwt Token</response> 
     /// <responde code="400">If the username or password are invalid</response>
     [HttpPost("")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult GenerateToken([FromForm] AuthTokenRequest authTokenRequest)
     {
         var user = DB.Users.FirstOrDefault(u => u.Email == authTokenRequest.Email);
@@ -61,6 +66,20 @@ public class AuthController: BaseController
         var token = tokenHandler.CreateToken(tokenDescription);
         var jwt = tokenHandler.WriteToken(token);
         return Ok(jwt);
+    }
+    /// <summary>
+    /// Return the user data
+    /// </summary>
+    /// <response code="200">Returns the user data</response>
+    [Authorize]
+    [HttpGet("me")]
+    public IActionResult Me()
+    {
+        var header = this.Request.Headers;
+        var token = header["Authorization"].ToString().Replace("Bearer ", "");
+        var mail = new JwtSecurityToken(token).Claims.First(c => c.Type == "sub").Value;
+        var user = DB.Users.FirstOrDefault(u => u.Email == mail);
+        return Ok(user);
     }
 
 }
--- BarsantiExplorer/Models/BarsantiDbContext.cs
+++ BarsantiExplorer/Models/BarsantiDbContext.cs
@@ -1,18 +1,24 @@
 ﻿using BarsantiExplorer.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Newtonsoft.Json.Bson;
+using System.Transactions;
 
 namespace BarsantiExplorer.Models
 {
     public class BarsantiDbContext : DbContext
     {
-
         public DbSet<Entities.Comment> Comments { get; set; }
         public DbSet<Entities.Trip> Trips { get; set; }
         public DbSet<Entities.TripType> TripTypes { get; set; }
         public DbSet<Entities.User> Users { get; set; }
         public BarsantiDbContext(DbContextOptions<BarsantiDbContext> options) : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
         }
     }
 }
