b7f9aa434ea378b7a449e8d1517befe17ce96745
jessyjackson <luconlorenzo@gmail.com>
2024-03-30 14:22:04 +0100

    Update 20240327093333_triggers.cs

--- BarsantiExplorer/Migrations/20240327093333_triggers.cs
+++ BarsantiExplorer/Migrations/20240327093333_triggers.cs
@@ -12,13 +12,24 @@ namespace BarsantiExplorer.Migrations
         {
             migrationBuilder.Sql(@"DROP TRIGGER IF EXISTS `CalculateAverage`;
                                 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-                                CREATE TRIGGER `CalculateAverage` AFTER INSERT ON `comments` FOR EACH ROW BEGIN
-                                UPDATE trips
-                                SET
-                                trips.RatingsNumber = trips.RatingsNumber + 1,
-                                trips.TotalRating = trips.TotalRating + NEW.Rating,
-                                trips.AverageRating = trips.TotalRating / trips.RatingsNumber
-                                WHERE Id = NEW.TripId;
+                                CREATE TRIGGER `CalculateAverage` AFTER UPDATE ON `comments` FOR EACH ROW
+                                BEGIN
+                                   IF NEW.status = 1 THEN
+                                       UPDATE trips
+                                       SET
+                                           RatingsNumber = RatingsNumber + 1,
+                                           TotalRating = TotalRating + NEW.Rating,
+                                           AverageRating = TotalRating / RatingsNumber
+                                       WHERE Id = NEW.TripId;
+                                   END IF;
+                                   IF NEW.DeletedAt <> OLD.DeletedAt then
+                                      UPDATE trips
+                                       SET
+                                           RatingsNumber = RatingsNumber - 1,
+                                           TotalRating = TotalRating - NEW.Rating,
+                                           AverageRating = TotalRating / RatingsNumber
+                                       WHERE Id = NEW.TripId;
+                                   END IF;
                                 END;
                                 SET SQL_MODE=@OLDTMP_SQL_MODE;");
         }
